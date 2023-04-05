542f03167df9d66c94a4ca769bd6beecd3b444d3
Jesper Madsen <jam@umbraco.dk>
2023-04-05 10:36:51 +0200

    dfhdf

--- src/CloudPublicAccess/Controllers/CloudPublicAccessController.cs
+++ src/CloudPublicAccess/Controllers/CloudPublicAccessController.cs
@@ -18,19 +18,16 @@ public class CloudPublicAccessController : UmbracoAuthorizedJsonController
     [HttpGet]
     public CloudPublicAccessSettings GetSettings()
     {
-        var portalLink = Environment.GetEnvironmentVariable("UMBRACO:CLOUD:IDENTITY:CLOUDRELAYENDPOINTBASEURL");
-        portalLink = portalLink.Replace("api.", "");
-        return CreateResponse(_basicAuthSettings, portalLink);
+        return CreateResponse(_basicAuthSettings);
     }
 
-    private CloudPublicAccessSettings CreateResponse(BasicAuthSettings basicAuthSettings, string portalLink)
+    private CloudPublicAccessSettings CreateResponse(BasicAuthSettings basicAuthSettings)
     {
         return new CloudPublicAccessSettings
         {
             Enabled = basicAuthSettings.Enabled,
             AllowedIPs = basicAuthSettings.AllowedIPs,
-            RedirectToLoginPage = basicAuthSettings.RedirectToLoginPage,
-            PortalLink = portalLink
+            RedirectToLoginPage = basicAuthSettings.RedirectToLoginPage
         };
     }
 }
@@ -42,5 +39,4 @@ public class CloudPublicAccessSettings
     public string[] AllowedIPs { get; init; } = Array.Empty<string>();
 
     public bool RedirectToLoginPage { get; init; }
-    public string PortalLink { get; init; }
 }
