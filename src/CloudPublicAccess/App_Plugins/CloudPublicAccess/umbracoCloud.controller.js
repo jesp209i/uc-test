79ea2bad018dce4a7d2fab575d39402a4ba49471
Jesper Madsen <jam@umbraco.dk>
2023-04-29 00:50:25 +0200

    environment name

--- src/CloudPublicAccess/App_Plugins/CloudPublicAccess/umbracoCloud.controller.js
+++ src/CloudPublicAccess/App_Plugins/CloudPublicAccess/umbracoCloud.controller.js
@@ -1,18 +1,19 @@
 (function (){
     'use strict';
     
-    function loader(eventsService, $compile, $rootScope){
+    function loader(eventsService, $compile, $rootScope, $location){
         eventsService.on('app.ready', function (){
             insertCloudLink();
         });
         
         function insertCloudLink(){
             var headerActions = angular.element(document).find('.umb-app-header__actions');
-            //console.log(headerActions);
+            console.log($location);
             
             const runme = true;
             
             if (runme && headerActions !== null && headerActions.length === 1){
+                
                 let umbIcon = document.createElement('umb-icon');
                 umbIcon.setAttribute('icon', "icon-cloud");
                 umbIcon.setAttribute('aria-hidden', 'true');
@@ -36,19 +37,40 @@
                 cloudButton.className = "umb-app-header__button btn-reset";
                 cloudButton.append(spanSrOnly, spanActionIcon);
                 
-                let item = document.createElement('li');
-                item.dataset['element'] = "global-cloud-portal-link";
-                item.className = 'umb-app-header__action';
-                item.append(cloudButton);
+                let listItem = document.createElement('li');
+                listItem.dataset['element'] = "global-cloud-portal-link";
+                listItem.className = 'umb-app-header__action';
+                listItem.append(cloudButton);
+
+                let listItemEnvironmentName = document.createElement('li');
+                listItemEnvironmentName.dataset['element'] = "global-environment-name";
+                listItemEnvironmentName.className = 'umb-app-header__action';
+                listItemEnvironmentName.style.fontWeight = "900";
+                listItemEnvironmentName.style.color = "#CCC";
+                listItemEnvironmentName.innerText = getEnvironment();
+
                 
-                headerActions[0].prepend(item);
-                $compile(item)($rootScope);
+                headerActions[0].prepend(listItem);
+                headerActions[0].prepend(listItemEnvironmentName);
+                $compile(listItem)($rootScope);
             }
         }
         
         function goToCloudPortal() {
             const cloudPortal = "https://s1.umbraco.io";
             window.open(cloudPortal, '_blank').focus();
+        }
+
+        function getEnvironment(){
+            const absUrl = $location.absUrl();
+            if (absUrl.includes("://localhost:"))
+                return "local";
+            if (absUrl.includes("://dev-"))
+                return "development";
+            if (absUrl.includes("://stage-"))
+                return "staging";
+
+            return "live";
         }
     }
     
